4d33b8eefabbecc8ed80721e213e473abe43e6c5
Leonardo Ruiz Bottura <92493176+LeonardoRuizB@users.noreply.github.com>
2023-08-22 16:45:49 -0300

    build: build project

---
+++ Cardapio Back-End/src/main/java/io/github/leonardoruizb/cardapio/CardapioApplication.java
@@ -0,0 +1,12 @@
+package io.github.leonardoruizb.cardapio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioApplication.class, args);
+	}
+}
---
+++ Cardapio Back-End/src/main/java/io/github/leonardoruizb/cardapio/food/FoodRequestDTO.java
@@ -0,0 +1,5 @@
+package io.github.leonardoruizb.cardapio.food;
+
+public record FoodRequestDTO(String title, String image, Integer price) {
+
+}
